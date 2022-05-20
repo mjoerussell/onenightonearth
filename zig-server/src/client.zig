1579009ea0d01fd93b7aa54fc39c0344887dd2d1
Mitch <mjoerussell@gmail.com>
2022-05-20 10:04:45 -0500

    Replacing old NetworkLoop with new EventLoop, cleaning up the WindowsClient, and adding a LinuxClient

--- zig-server/src/client.zig
+++ zig-server/src/client.zig
@@ -8,78 +8,15 @@ const winsock = @import("./winsock.zig");
 
 const Allocator = std.mem.Allocator;
 
-pub const Client = if (builtin.os.tag == .windows)
-    WindowsClient
-else
-    DefaultClient;
+const is_windows = builtin.os.tag == .windows;
 
-pub const NetworkLoop = struct {
-    pub const ResumeNode = struct {
-        frame: anyframe,
-        overlapped: os.windows.OVERLAPPED,
-        is_resumed: bool = false,
-    };
-
-    worker_threads: []std.Thread,
-    io_port: os.windows.HANDLE,
-    is_running: bool = false,
-
-
-    pub fn init(loop: *NetworkLoop, allocator: Allocator) !void {
-        loop.is_running = true;
-        loop.io_port = try os.windows.CreateIoCompletionPort(os.windows.INVALID_HANDLE_VALUE, null, undefined, undefined);
-    
-        loop.worker_threads = try allocator.alloc(std.Thread, std.Thread.getCpuCount() catch 4);
-        for (loop.worker_threads) |*thread| {
-            thread.* = try std.Thread.spawn(.{}, NetworkLoop.run, .{loop});
-        }
-    }
-
-    pub fn deinit(loop: *NetworkLoop) void {
-        loop.is_running = false;
-        os.windows.CloseHandle(loop.io_port);
-    }
-
-    fn run(loop: *NetworkLoop) !void {
-        while (loop.is_running) {
-            var completion_key: usize = undefined;
-            var overlapped: ?*os.windows.OVERLAPPED = undefined;
-            _ = winsock.getQueuedCompletionStatus(loop.io_port, &completion_key, &overlapped) catch |err| switch (err) {
-                error.Aborted => return,
-                error.Eof => {},
-                else => continue,
-            };
-
-            if (overlapped) |o| {
-                var resume_node = @fieldParentPtr(ResumeNode, "overlapped", o);
-                if (@cmpxchgStrong(bool, &resume_node.is_resumed, false, true, .SeqCst, .SeqCst) == null) {
-                    resume resume_node.frame;
-                }
-            }
-        }
-    }
-
-    fn register(loop: *NetworkLoop, socket: os.socket_t) !void {
-        _ = try os.windows.CreateIoCompletionPort(socket, loop.io_port, undefined, 0);
-    }
-
-    inline fn createResumeNode(frame: anyframe) ResumeNode {
-        return .{ 
-            .frame = frame,
-            .overlapped = os.windows.OVERLAPPED{
-                .Internal = 0,
-                .InternalHigh = 0,
-                .DUMMYUNIONNAME = .{
-                    .DUMMYSTRUCTNAME = .{
-                        .Offset = 0,
-                        .OffsetHigh = 0,
-                    },
-                },
-                .hEvent = null,
-            }
-        };
-    }
+pub const Client = switch (builtin.os.tag) {
+    .windows => WindowsClient,
+    .linux => LinuxClient,
+    else => DefaultClient,
 };
+
+pub const NetworkLoop = @import("event_loop.zig").EventLoop;
 
 const WindowsClient = struct {
     pub const Reader = std.io.Reader(WindowsClient, winsock.RecvError, recv);
@@ -93,16 +30,6 @@ const WindowsClient = struct {
             .socket = socket,
             .loop = loop,
         };
-    }
-
-    pub fn connectToAddress(loop: *NetworkLoop, address: net.Address) !Client {
-        const flags = os.windows.ws2_32.WSA_FLAG_OVERLAPPED | os.windows.ws2_32.WSA_FLAG_NO_HANDLE_INHERIT;
-        const sockfd = try os.windows.WSASocketW(@intCast(i32, address.any.family), @as(i32, os.SOCK.STREAM), @as(i32, os.IPPROTO.TCP), null, 0, flags);
-        errdefer os.closeSocket(sockfd);
-
-        try os.connect(sockfd, &address.any, address.getOsSockLen());
-
-        return try Client.init(loop, sockfd);
     }
 
     pub fn deinit(client: *Client) void {
@@ -123,12 +50,7 @@ const WindowsClient = struct {
     pub fn send(client: Client, data_buffer: []const u8) winsock.SendError!usize {
         var resume_node = NetworkLoop.createResumeNode(@frame());
         suspend {
-            if (winsock.wsaSend(client.socket, data_buffer, &resume_node.overlapped)) {
-                // if (@cmpxchgStrong(bool, &resume_node.is_resumed, false, true, .SeqCst, .SeqCst) == null) {
-                //     std.log.debug("Got send result immediately, resuming frame {*}\n", .{&resume_node.frame});
-                //     resume @frame();
-                // }
-            } else |_| {}
+            winsock.wsaSend(client.socket, data_buffer, &resume_node.overlapped) catch {};
         }
         return try winsock.wsaGetOverlappedResult(client.socket, &resume_node.overlapped);
     }
@@ -136,15 +58,60 @@ const WindowsClient = struct {
     pub fn recv(client: Client, buffer: []u8) winsock.RecvError!usize {
         var resume_node = NetworkLoop.createResumeNode(@frame());
         suspend {
-            if (winsock.wsaRecv(client.socket, buffer, &resume_node.overlapped)) {
-                // if (@cmpxchgStrong(bool, &resume_node.is_resumed, false, true, .SeqCst, .SeqCst) == null) {
-                //     std.log.debug("Got recv result immediately, resuming frame (node = {*})\n", .{&resume_node});
-                //     // resume_node.is_resumed = true;
-                //     resume @frame();
-                // }
-            } else |_| {}
+            winsock.wsaRecv(client.socket, buffer, &resume_node.overlapped) catch {};
         }
         return try winsock.wsaGetOverlappedResult(client.socket, &resume_node.overlapped);
+    }
+
+    pub fn reader(self: Client) Reader {
+        return .{ .context = self };
+    }
+
+    pub fn writer(self: Client) Writer {
+        return .{ .context = self };
+    }
+};
+
+const LinuxClient = struct {
+    const RecvError = error{RecvErr, SubmitErr};
+    const SendError = error{SendErr, SubmitErr};
+    pub const Reader = std.io.Reader(LinuxClient, RecvError, recv);
+    pub const Writer = std.io.Writer(LinuxClient, SendError, send);
+    socket: os.socket_t,
+    loop: *NetworkLoop,
+
+    pub fn init(loop: *NetworkLoop, socket: os.socket_t) !Client {
+        return Client{
+            .socket = socket,
+            .loop = loop,
+        };
+    }
+
+    pub fn deinit(client: *Client) void {
+        os.close(client.socket);
+    }
+
+    pub fn send(client: Client, data_buffer: []const u8) SendError!usize {
+        const flags = os.linux.IOSQE_ASYNC;
+        var resume_node = NetworkLoop.createResumeNode(@frame());
+
+        suspend {
+            _ = client.loop.io_uring.send(@intCast(u64, @ptrToInt(&resume_node)), client.socket, data_buffer, flags) catch return error.SendErr;
+            _ = client.loop.io_uring.submit() catch return error.SubmitErr;
+        }
+
+        return std.math.min(resume_node.bytes_worked, data_buffer.len);
+    }
+
+    pub fn recv(client: Client, buffer: []u8) RecvError!usize {
+        const flags = os.linux.IOSQE_ASYNC;
+        var resume_node = NetworkLoop.createResumeNode(@frame());
+        suspend {
+            _ = client.loop.io_uring.recv(@intCast(u64, @ptrToInt(&resume_node)), client.socket, buffer, flags) catch return error.RecvErr;
+            _ = client.loop.io_uring.submit() catch return error.SubmitErr;
+        }
+
+        return resume_node.bytes_worked;
     }
 
     pub fn reader(self: Client) Reader {
