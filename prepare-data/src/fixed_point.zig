73e87be4cdc23b95e439cda040d258e79f260eee
Mitch <mjoerussell@gmail.com>
2022-05-24 15:26:24 -0500

    Adding a FixedPoint struct to night-math and prepare-data in order to store coordinates as 16 bit fixed point numbers instead of 32 bit floating point numbers. This cuts the amount of data needed to transfer nearly in half.

---
+++ prepare-data/src/fixed_point.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn FixedPoint(comptime Int: type, comptime fractional_bits: u8) type {
+    if (!std.meta.trait.isIntegral(Int)) @compileError("FixedPoint requires an integer type as the target conversion");
+
+    return packed struct {
+        const Self = @This();
+
+        pub inline fn fromFloat(float_value: f32) Int {
+            return @floatToInt(Int, float_value * (1 << fractional_bits));
+        }
+
+        pub inline fn toFloat(fixed: Int) f32 {
+            return @intToFloat(f32, fixed) / @intToFloat(f32, @as(Int, 1) << fractional_bits);
+        }
+    };
+}
