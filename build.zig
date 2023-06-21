f0725adb81d0731e25ca8af1702536efde090ab9
Mitch <mjoerussell@gmail.com>
2023-06-20 22:31:59 -0500

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,40 +1,9 @@
 const std = @import("std");
 const build = std.build;
+const Build = build.Build;
 
-pub fn link(exe: *build.LibExeObjStep, path: []const u8) void {
-    exe.addPackage(.{
-        .name = "tortie",
-        .path = build.FileSource.relative(path),
+pub fn build(b: *std.build.Build) !void {
+    _ = b.addModule("tortie", .{
+        .source_file = .{ .path = "src/tortie.zig" },
     });
-}
-
-pub fn build(b: *std.build.Builder) !void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
-
-    const exe = b.addExecutable("tortie", "src/main.zig");
-    
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
-
-    exe.single_threaded = is_single_threaded orelse false;
-
-    exe.addPackagePath("http", "http/src/lib.zig");
-
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }
