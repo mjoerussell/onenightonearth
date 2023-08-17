9db3c67f8c9eb9101668664b1447ba21b758f770
Mitch <mjoerussell@gmail.com>
2023-08-17 09:03:54 -0500

    Reset the length of the request/response buffers when zeroing the client (only WindowsClient right now)

--- src/server.zig
+++ src/server.zig
@@ -72,11 +72,13 @@ pub const WindowsClient = struct {
         client.request = undefined;
         if (client.request_buffer.capacity > 1024) {
             client.request_buffer.shrinkAndFree(1024);
+            client.request_buffer.items.len = 0;
         }
 
         client.response = undefined;
         if (client.request_buffer.capacity > 1024) {
             client.response_buffer.shrinkAndFree(1024);
+            client.response_buffer.items.len = 0;
         }
 
         client.overlapped = .{
