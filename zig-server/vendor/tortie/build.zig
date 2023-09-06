223ec4e9d8edeb7735125d57f4b4f7a8e22c9154
Mitch <mjoerussell@gmail.com>
2023-09-06 09:20:05 -0500

    Remove vendored tortie from git tree

--- zig-server/vendor/tortie/build.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const Build = std.Build;
-
-pub fn build(b: *Build) !void {
-    _ = b.addModule("tortie", .{
-        .source_file = .{ .path = "src/tortie.zig" },
-    });
-}
