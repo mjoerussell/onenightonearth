aaf33e0fbc1904b4c6bc17264de43c0486104a95
Mitch <mjoerussell@gmail.com>
2024-01-19 13:48:49 -0600

    * Update `build.zig` to work with the latest Zig changes * Update tortie dependency

--- server/build.zig
+++ server/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 const vendor_dir = "vendor/";
 
-pub fn build(b: *std.build.Builder) !void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -17,25 +17,23 @@ pub fn build(b: *std.build.Builder) !void {
     const target_linux = b.option(bool, "target-linux", "Build for the default linux target instead of the native target") orelse false;
     const use_vendored_deps = b.option(bool, "use-vendor", "Use vendored versions of listed dependencies (put dependencies under a vendor/ directory)") orelse false;
 
-    const default_linux_target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "x86_64-linux" });
+    const default_linux_target = try std.Target.Query.parse(.{ .arch_os_abi = "x86_64-linux" });
 
     const exe = b.addExecutable(.{
         .name = "zig-server",
         .root_source_file = .{ .path = "src/main.zig" },
         .optimize = mode,
-        .target = if (target_linux) default_linux_target else target,
+        .target = if (target_linux) b.resolveTargetQuery(default_linux_target) else target,
+        .single_threaded = is_single_threaded orelse false,
     });
 
-    if (!use_vendored_deps) {
-        const tortie_dep = b.dependency("tortie", .{});
-        exe.addModule("tortie", tortie_dep.module("tortie"));
-    } else {
-        exe.addAnonymousModule("tortie", .{ .source_file = .{ .path = vendor_dir ++ "tortie/src/tortie.zig" }, .dependencies = &.{} });
-    }
+    const tortie_module = if (use_vendored_deps)
+        b.createModule(.{ .root_source_file = .{ .path = vendor_dir ++ "tortie/src/tortie.zig" } })
+    else
+        b.dependency("tortie", .{}).module("tortie");
+    exe.root_module.addImport("tortie", tortie_module);
 
     b.installArtifact(exe);
-
-    exe.single_threaded = is_single_threaded orelse false;
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
